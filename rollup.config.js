dd6fc2746d05a07a4eeb94b791b4efab4ccf506f
Eddcakes <eddadamson@gmail.com>
2020-10-27 12:11:48 +0000

    update svelte/sapper & remove cypress

--- rollup.config.js
+++ rollup.config.js
@@ -13,8 +13,8 @@ const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-  (warning.code === 'CIRCULAR_DEPENDENCY' &&
-    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  (warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+  (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
   onwarn(warning);
 
 export default {
@@ -86,6 +86,7 @@ export default {
       }),
       svelte({
         generate: 'ssr',
+        hydratable: true,
         dev,
       }),
       resolve({
